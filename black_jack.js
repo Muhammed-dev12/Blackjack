cfff57e1c16b560e51a28c58cfca052197ee6ebb
Muhammed-dev12 <muhammedmisbah1214@gmail.com>
2025-11-10 17:52:34 +0300

    Update black_jack.js
    removing unnecessary codes

--- black_jack.js
+++ black_jack.js
@@ -65,13 +65,3 @@ function newCard () {
     }
     renderGame()
 }
-
-let hands = ["rock", "paper", "scissor"]
-
-function randomHands () {
-    let randomIndex = Math.floor(Math.random() * 3)
-    
-    return hands[randomIndex]
-}
-
-console.log(randomHands())
